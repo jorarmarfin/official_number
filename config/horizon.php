396d273484a4bb7916cc2773a4d16022c3c26b80
Luis Mayta <luis.mayta@gmail.com>
2026-01-09 15:11:52 -0500

    variable de cola

--- config/horizon.php
+++ config/horizon.php
@@ -199,7 +199,7 @@ return [
     'defaults' => [
         'supervisor-1' => [
             'connection' => 'redis',
-            'queue' => ['default'],
+            'queue' => [env('QUEUE_NAME', 'default')],
             'balance' => 'auto',
             'autoScalingStrategy' => 'time',
             'maxProcesses' => 1,
